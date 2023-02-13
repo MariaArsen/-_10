8227949da8c8e0cee7e18e422b551d5c22b501a4
Солнце <@LAPTOP-DPKNJGCI>
2023-02-13 17:47:22 +0300

    Добавьте файлы проекта.

---
+++ СМ_10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace СМ_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            Console.WriteLine("Введите последовательно градусы,минуты,секунды угла");
+            double gradus_ = Convert.ToInt32(Console.ReadLine());
+            double min_ = Convert.ToInt32(Console.ReadLine());
+            double sec_ = Convert.ToInt32(Console.ReadLine());
+
+            Ugol ugol = new Ugol (gradus_, min_, sec_);
+            
+            double totalRadians = ugol.ToRadians();
+            Console.WriteLine("Абсолютное значение радиан {0:f3}",totalRadians);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ СМ_10/Ugol.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace СМ_10
+{
+    class Ugol
+    {
+        double gradus;
+        double min;
+        double sec;
+
+        public double Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+                while (value > 360)
+                {
+                   gradus = (value - 360)%360;
+                    
+                }  
+                if (value < 360)
+                {
+                  gradus = value % 360;
+                }    
+                else if (value == 360)
+                {
+                   gradus = 0;
+                }
+            }
+        }
+        public double Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                Gradus += value / 60;
+                min = value % 60;
+            }
+        }
+        public double Sec
+        {
+            get
+            {
+                return sec;
+            }
+            set
+            {
+                Min += value / 60;
+                sec = value % 60;
+            }
+        }
+
+        public Ugol(double gradus, double min, double sec)
+        {
+            this.gradus = gradus;
+            this.min = min;
+            this.sec = sec;
+        }
+
+        public double ToRadians()
+        {
+            return ((Math.Abs(gradus) + ((Math.Abs(min) + Math.Abs(sec) / 60) / 60)) * (Math.PI/180));
+        }
+    }
+}
